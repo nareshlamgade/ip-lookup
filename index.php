fbdeb02c89f52fb9b0e9ba360e7127a18d2aad09
Naresh LamGade <nareshlamgade@gmail.com>
2015-01-10 15:19:00 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head profile="http://gmpg.org/xfn/11">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="description" content="Check Your Current IP Address" />
+<meta name="keywords" content="IP Address , IP , Check Your IP , IP-Address , current ip  " />
+
+		<meta name="Date" content="2008" />
+		<meta name="Distribution" content="Copyright 2003-2010 ." />
+		<title> | Your Current IP Address |</title>
+		<link href="style.css" type="text/css" rel="stylesheet">
+		<script type="text/javascript" src="js.js"></script>
+
+	</head>
+	<body>
+<div id="content">
+		<h2>Your Current IP Address Is</h2>
+<?php
+if ($_SERVER["HTTP_X_FORWARDED_FOR"] != ""){
+     $IP = $_SERVER["HTTP_X_FORWARDED_FOR"];
+     $proxy = $_SERVER["REMOTE_ADDR"];
+     $host = @gethostbyaddr($_SERVER["HTTP_X_FORWARDED_FOR"]);
+}else{
+     $IP = $_SERVER["REMOTE_ADDR"];
+     $host = @gethostbyaddr($_SERVER["REMOTE_ADDR"]);
+}
+?>		
+		<h1><?=$IP;?><br /><small><?=$host;?><br><? echo ($proxy)?$proxy:'';?></small></h1>
+		<div id="more">
+<?php
+$k = array(user_agent=>HTTP_USER_AGENT,remote_port=>REMOTE_PORT,request_method=>REQUEST_METHOD,protocol=>SERVER_PROTOCOL);
+foreach ($k as $h=>$v)echo "<b>".str_replace("_"," ",$h)."</b> : $_SERVER[$v]<br>";
+?>
+		</div>
+		
+	</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<font size="2"><a href="http://nareshlamgade.com.np" target="blank">Back To Index</a></font><br>
+
+	</body>
+</html>
